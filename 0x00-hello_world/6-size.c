9afe278e4a9650cf71a6b2d7a6ca6aac187bd9c1
sonie-marsha <soniemarsha@gmail.com>
2023-09-07 08:57:43 +0300

    hello, world

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	char a;
+	int i;
+	long int j;
+	long long int k;
+	float d;
+
+	printf("Size of a char: &d byte(s)\n", sizeof(a));
+	printf("Size of an int: %d byte(s)\n", sizeof(i));
+	printf("Size of a long int: %d byte(s)\n", sizeof(j));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(j));
+	printf("Size of a float: %d byte(s)\n", sizeof(k));
+
+	return (0);
+}
