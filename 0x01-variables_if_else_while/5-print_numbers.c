432a037219b6e9de123de2c9a68536f2cb20ea5a
sonie-marsha <soniemarsha@gmail.com>
2023-09-08 16:26:15 +0300

    q and e

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - prints out 0-9
+ *
+ *Return: (0);
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+		printf("%d\n", num);
+
+	return (0);
+}
