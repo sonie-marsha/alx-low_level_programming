380fe4d0506cb2e1ba6a1c841213205f32c4f3fe
sonie-marsha <soniemarsha@gmail.com>
2023-09-12 16:52:23 +0300

    alphabet in lower case 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,7 +11,9 @@ void print_alphabet_x10(void)
 	int i, alphabet;
 
 	for (i = 0; i < 10; i++)
+	{
 		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 			_putchar(alphabet);
-	_putchar('\n');
+		_putchar('\n');
+	}
 }
