2d0a9642cbdffc864a669480d54e700a2bc6ff05
sonie-marsha <soniemarsha@gmail.com>
2023-09-13 13:34:22 +0300

    jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,8 +10,8 @@ void jack_bauer(void)
 {
 	int hr, min;
 
-	for (hr = 0; hr <= 24; hr++)
-		for (min = 0; min <= 60; min++)
+	for (hr = 0; hr < 24; hr++)
+		for (min = 0; min < 60; min++)
 		{
 			_putchar((hr / 10) + '0');
 			_putchar((hr % 10) + '0');
