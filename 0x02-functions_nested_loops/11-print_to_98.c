ea91aacbea76c4c2548d9e6ef284fd5000076fb8
sonie-marsha <soniemarsha@gmail.com>
2023-09-13 16:33:24 +0300

    natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,14 +15,15 @@ void print_to_98(int n)
 		{
 			printf("%d, ", n);
 		}
-		else if (n > 98)
-		{
-			for (; n > 98; n--)
-			{
-				printf("%d ", n);
-			}
-		}
 	}
+	else (n > 98)
+	     {
+		     for (; n > 98; n--)
+		     {
+			     printf("%d ", n);
+		     }
+	     }
 
 	printf("%d\n", n);
 }
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -13,5 +13,6 @@ void jack_bauer(void);
 void times_table(void);
 int add(int a, int b);
 void print_times_table(int n);
+void print_to_98(int n);
 
 #endif
