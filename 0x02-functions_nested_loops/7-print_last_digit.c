8b563219e55b5b9f7aedbcc439d61e6ed441b4f1
sonie-marsha <soniemarsha@gmail.com>
2023-09-13 12:40:06 +0300

    absolute number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *print_last_digit - prints last digit of a num
+ *
+ *Return: 0
+ */
+
+int print_last_digit(int)
+{
+	int n;
+	int last_digit = n % 10;
+
+	_putchar('last_digit');
+
+	return (0);
+}
