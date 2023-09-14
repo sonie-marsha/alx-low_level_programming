1aa75043955a7dfd126021fc4497839e4674c255
sonie-marsha <soniemarsha@gmail.com>
2023-09-14 08:54:15 +0300

    fib

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,12 +9,12 @@
 int main(void)
 {
 	unsigned long int fib1, fib2, nextFib;
+	int i;
 
 	fib1 = 1;
 	fib2 = 1;
 
 	printf("1");
-	int i;
 
 	for (i = 2; i <= 97; i++)
 	{
@@ -23,7 +23,7 @@ int main(void)
 		nextFib = fib1 + fib2;
 
 		fib1 = fib2;
-		fib2 = nextFib:
+		fib2 = nextFib;
 	}
 
 	printf("\n");
