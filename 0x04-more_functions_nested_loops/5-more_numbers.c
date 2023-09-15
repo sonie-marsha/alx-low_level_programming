6088b0d50f84d711e494b07dcb1aaddac8a3a367
sonie-marsha <soniemarsha@gmail.com>
2023-09-15 08:19:36 +0300

    more functions

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -16,10 +16,8 @@ void more_numbers(void)
 		for (number = 0; number <= 14; number++)
 		{
 			if (number > 9)
-			{
 				_putchar((number / 10) + '0');
-				_putchar((number % 10) + '0');
-			}
+			_putchar((number % 10) + '0');
 		}
 		_putchar('\n');
 	}
