5b4d8796acaebca716432a4ef2c30735be478ac6
sonie-marsha <soniemarsha@gmail.com>
2023-09-26 22:58:17 +0300

    double ptr

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *set_string - sets value of a ptr to char
+ *@s: double ptr
+ *@to: ptr to set s to
+ *
+ *Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
