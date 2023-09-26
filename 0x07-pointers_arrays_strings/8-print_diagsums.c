4f044a096c5f5b9608c7967ca1af411ddffbd8ae
sonie-marsha <soniemarsha@gmail.com>
2023-09-26 21:54:33 +0300

    fixed diagsum

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -13,12 +13,13 @@ void print_diagsums(int *a, int size)
 {
 	int sum1 = 0;
 	int sum2 = 0;
-	int row, col;
 
-	for (row = 0; i < size; row++)
+	int i;
+
+	for (i = 0; i < size; i++)
 	{
-		sum1 += a[row * size + row];
-		sum2 += a[row * size + (size -1 - row)];
+		sum1 += a[i * size + i];
+		sum2 += a[i * size + (size -1 - i)];
 	}
 
 	printf("%d, %d\n", sum1, sum2);
