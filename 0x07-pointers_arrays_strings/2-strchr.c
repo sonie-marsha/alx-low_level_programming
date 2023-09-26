1bfefb3a3cfa5941132503fd88b1aba21404d371
sonie-marsha <soniemarsha@gmail.com>
2023-09-26 20:43:35 +0300

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,1 +1,21 @@
 #include "main.h"
+
+/**
+ *_strchr - prints out the first occurence of a char
+ *@s: string
+ *@c: character
+ *
+ *Return: ptr to c in str
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+
+	return (NULL);
+}
