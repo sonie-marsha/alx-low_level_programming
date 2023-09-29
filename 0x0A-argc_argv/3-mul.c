93f2f709bb8351b49b9667a8398be8aca387bced
sonie-marsha <soniemarsha@gmail.com>
2023-09-29 22:06:28 +0300

    mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ *main - entry
+ *@argc: count
+ *@argv: vectors
+ *
+ *Return: 0 or 1 if count != 3
+ */
+
+int main(int argc, char **argv)
+{
+	int i, j, k;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	i = atoi(argv[1]);
+	j = atoi(argv[2]);
+	k = i * j;
+
+	printf("%d\n", k);
+	return (0);
+}
