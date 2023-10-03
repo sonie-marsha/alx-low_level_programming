c0ce06c4cc9d390877d350d0871e5f93f24de8f8
sonie-marsha <soniemarsha@gmail.com>
2023-10-03 09:50:44 +0300

    create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	*arr = malloc(size * sizeof(char));
+	*arr = (int *)malloc(size * sizeof(char));
 
 	if (arr == NULL)
 		return (NULL);
