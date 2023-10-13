77f4b4cdefeb9651250af77bbdc731807a4c6ef6
sonie-marsha <soniemarsha@gmail.com>
2023-10-13 14:35:57 +0300

    int index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+ *int_index - returns the index of the first element for which
+ *cmp function does not return 0
+ *@size: number of elements in an array
+ *@cmp: ptr to the function to be used to compare values
+ *@array: an array of elements
+ *
+ *Return: int
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+
+	return (-1);
+}
