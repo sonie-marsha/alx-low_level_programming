6219aeaf2cce9efedcdb4c95633f5375a1e489d4
sonie-marsha <soniemarsha@gmail.com>
2023-10-13 15:40:50 +0300

    op

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,19 +1,51 @@
 #include "3-calc.h"
 
+/**
+ *op_add - addition
+ *@a: num1
+ *@b: num2
+ *
+ *Return: sum
+ */
+
 int op_add(int a, int b)
 {
 	return (a + b);
 }
+
+/**
+ *op_sub - subtraction
+ *@a: num1
+ *@b: num2
+ *
+ *Return: subtraction
+ */
 
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
+/**
+ *op_mul - multiplication
+ *@a: num1
+ *@b: num2
+ *
+ *Return: multiplication
+ */
+
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
+
+/**
+ *op_mul - division
+ *@a: num1
+ *@b: num2
+ *
+ *Return: division
+ */
 
 int op_div(int a, int b)
 {
