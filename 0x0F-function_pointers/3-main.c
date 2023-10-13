e3572e4dbea84ce4ad72f98515ce5ecc81972786
sonie-marsha <soniemarsha@gmail.com>
2023-10-13 16:29:29 +0300

    op

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,1 +1,15 @@
 #include "calc.h"
+
+/**
+ *main - entry point
+ *@argc: no of args
+ *@argv: vector
+ *
+ *Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	int (*operation)(int, int);
+}
